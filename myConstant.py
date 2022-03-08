29b45aa31922d4dc1411530f0bb4660416ce014a
dinglongxiang <dinglongxiang@meituan.com>
2022-03-08 10:24:46 +0800

    constant part2-index

--- myConstant.py
+++ myConstant.py
@@ -35,7 +35,44 @@ def base():
     # 找到最大值的index
     print(tf.argmax(a))
 
+def myIndex():
+
+    # 单轴
+    rank_1_tensor = tf.constant([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+    print(rank_1_tensor.numpy())
+
+    print("First:", rank_1_tensor[0].numpy())
+    print("Second:", rank_1_tensor[1].numpy())
+    print("Last:", rank_1_tensor[-1].numpy())
+
+    print("每个元素:", rank_1_tensor[:].numpy())
+    print("前四个元素:", rank_1_tensor[:4].numpy())
+    print("第四个到最后的元素:", rank_1_tensor[4:].numpy())
+    print("第二到七个元素:", rank_1_tensor[2:7].numpy())
+    print("每%3的元素（每第三个元素）:", rank_1_tensor[::3].numpy())
+    print("逆序:", rank_1_tensor[::-1].numpy())
+
+    # 多轴（同单轴）
+    rank_2_tensor = tf.constant([[1,2],[3,4]])
+    print(rank_2_tensor)
+    print(rank_2_tensor[1, 1])
+    print(rank_2_tensor[1, 1].numpy())
+
+    print("第二行的全部:", rank_2_tensor[1, :].numpy())
+    print("第二列的全部:", rank_2_tensor[:, 1].numpy())
+    print("最后一行:", rank_2_tensor[-1, :].numpy())
+    print("First item in last column:", rank_2_tensor[0, -1].numpy())
+    print("Skip the first row:")
+    print(rank_2_tensor[1:, :].numpy(), "\n")
+
+def reshape():
+    var_x = tf.Variable(tf.constant([[1], [2], [3]]))
+    print(var_x.shape)
+
+
 
 if __name__ == '__main__':
     # estimate_constant()
-    base()
+    # base()
+    # myIndex()
+    reshape()
